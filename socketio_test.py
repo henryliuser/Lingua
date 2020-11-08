ca45abbd502577654d5460fd6f0939aa26dc6103
Henry Liu <henryliuser@gmail.com>
2020-11-07 17:08:48 -0800

    users can connect with each other

--- socketio_test.py
+++ socketio_test.py
@@ -1,17 +1,87 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, send, emit
+from uuid import uuid4
+from queue import Queue
+from flask_socketio import join_room, leave_room
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+users_online = {}
+lang_queues = {}
+languages = ["English", "Spanish", "French"]
+for lang in languages:
+    lang_queues[lang] = Queue()
+
+class User:
+    def __init__(self, sid: str, name: str, language: str):
+        self.sid = sid
+        self.name = name
+        self.language = language
+        self.partner = None
+    def connect(self, part) -> None:
+        self.partner = part
+        part.partner = self
+        print(f"{self.name} CONNECTED TO {part.name}")
+        emit("found", room=part.sid)
+    def search(self):
+        temp = lang_queues[self.language].queue
+        if not lang_queues[self.language].empty() and not temp[0] is self:
+            u = lang_queues[self.language].get()
+            self.connect(u)
+            return True
+        return False
+
+
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    return render_template("henrytest.html")
 
-@socketio.on("message")
+@app.route('/searching')
+def searching():
+    return render_template("searching.html")
+
+@socketio.on("connect")
+def on_connect():
+    emit("SID", request.sid)
+
+@socketio.on("chat message")
 def handle_message(message):
-    print('received message: '+ message)
+    print(message)
+
+@socketio.on("new user")
+def new_user(sid, name, language):
+    u = User(sid, name, language)
+    users_online[sid] = u
+    if not u.search():
+        emit("loop search")
+        lang_queues[language].put(u)
+    else:
+        emit("found")
+    emit("user created", (sid, name, language))
+    print(f"Welcome {name}, speaking {language}, ID: {sid}!")
+
+from flask_socketio import join_room, leave_room
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', room=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', room=room)
+
+@socketio.on("search")
+def search(sid):
+    if users_online[sid].search():
+       emit("found")
 
 if __name__ == '__main__':
     socketio.run(app)
